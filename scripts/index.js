6a836b0189304af6e0165cd14cd7415693b8dd0d
Beastoloch <maks.serebrov.94@gmail.com>
2023-06-25 05:10:46 +0700

    feat/рефактор ООП

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const popupProfile = document.querySelector('#edit-popup');
 const popupAdding = document.querySelector('#add-popup');
-const popupImage = document.querySelector('#image-popup');
 const profile = document.querySelector('.profile');
 const profileBtn = profile.querySelector('.profile__edit-button');
 const addingBtn = profile.querySelector('.profile__add-button');
@@ -12,28 +11,27 @@ const inputJob = formProfile.querySelector('.form__input_type_job');
 const formAdding = popupAdding.querySelector('.form');
 const inputPlace = formAdding.querySelector('.form__input_type_place');
 const inputImage = formAdding.querySelector('.form__input_type_image');
-const imageTitle = popupImage.querySelector('.popup__image-title');
-const imageSrc = popupImage.querySelector('.popup__image');
 const cardContainer = document.querySelector('.elements');
+import Card from './card.js';
+import FormValidator from './FormValidator.js';
 
-const openPopup = (popup) => {
-    openErrorCheck(popup.querySelector('.form'));
+export const openPopup = (popup) => {
     document.addEventListener('keydown', setEscListener);
     document.addEventListener('mousedown', setOverlayListener);
     popup.classList.add('popup_opened');
 }
 
-const openErrorCheck = (formElement) => {
-    if(formElement){
-        const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-        inputList.forEach((inputElement) => {
-            hideInputError({inputErrorClass: 'form__input_type_error', errorClass: 'form__input-error_active'},
-                formElement, inputElement);
-            toggleButtonState({inactiveButtonClass: 'popup__save-button_inactive'}, inputList,
-                formElement.querySelector('.popup__save-button'));
-        });
-    }
-}
+// const openErrorCheck = (formElement) => {
+//     if(formElement){
+//         const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+//         inputList.forEach((inputElement) => {
+//             hideInputError({inputErrorClass: 'form__input_type_error', errorClass: 'form__input-error_active'},
+//                 formElement, inputElement);
+//             toggleButtonState({inactiveButtonClass: 'popup__save-button_inactive'}, inputList,
+//                 formElement.querySelector('.popup__save-button'));
+//         });
+//     }
+// }
 
 const openEdit = () => {
     inputName.value = `${profileName.textContent}`;
@@ -47,6 +45,10 @@ const openAdd = () => {
 }
 
 const closePopup = (popup) => {
+    const formCheck = popup.querySelector('.form');
+    if (formCheck){
+        Validation();
+    }
     popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', setEscListener);
     document.removeEventListener('click', setOverlayListener);
@@ -79,35 +81,56 @@ const submitAdd = (evt) => {
 }
 
 const renderCard = (cardObj) => {
-    const cardElement = createCard(cardObj);
+    const card = new Card(cardObj);;
+    const cardElement = card.generateCard();
     cardContainer.prepend(cardElement);
 }
 
-const createCard = (cardObj) => {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-    const cardImage = cardElement.querySelector('.element__image');
-    cardElement.querySelector('.element__title').textContent = cardObj.name;
-    cardImage.src = cardObj.link;
-    cardImage.alt = cardObj.name;
-    cardElement.querySelector('.element__like-button').addEventListener('click', evt => {
-        evt.target.classList.toggle('element__like-button_active')
-    })
-    cardElement.querySelector('.element__delete-button').addEventListener('click', () => {
-        cardElement.remove();
-    })
-    cardElement.querySelector('.element__image').addEventListener('click', () => {
-        openPopup(popupImage);
-        imageTitle.textContent = cardObj.name;
-        imageSrc.src = cardObj.link;
-        imageSrc.alt = cardObj.name;
-    })
-    return cardElement;
-}
+// const createCard = (cardObj) => {
+//     const cardTemplate = document.querySelector('#card-template').content;
+//     const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+//     const cardImage = cardElement.querySelector('.element__image');
+//     cardElement.querySelector('.element__title').textContent = cardObj.name;
+//     cardImage.src = cardObj.link;
+//     cardImage.alt = cardObj.name;
+//     cardElement.querySelector('.element__like-button').addEventListener('click', evt => {
+//         evt.target.classList.toggle('element__like-button_active')
+//     })
+//     cardElement.querySelector('.element__delete-button').addEventListener('click', () => {
+//         cardElement.remove();
+//     })
+//     cardElement.querySelector('.element__image').addEventListener('click', () => {
+//         openPopup(popupImage);
+//         imageTitle.textContent = cardObj.name;
+//         imageSrc.src = cardObj.link;
+//         imageSrc.alt = cardObj.name;
+//     });
+//     return cardElement;
+// }
 
+import {initialCards} from './initial-cards.js';
 initialCards.forEach(item => {
     renderCard(item);
-})
+});
+
+const config = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.popup__save-button',
+    inactiveButtonClass: 'popup__save-button_inactive',
+    inputErrorClass: 'form__input_type_error',
+    errorClass: 'form__input-error_active'
+};
+
+const Validation = () =>{
+    const formList = Array.from(document.querySelectorAll('.form'));
+    formList.forEach((formElement) => {
+        const validator = new FormValidator(config, formElement);
+        validator.enableValidation();
+    });
+}
+
+Validation();
 
 profileBtn.addEventListener('click', openEdit);
 addingBtn.addEventListener('click', openAdd);
--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
     {
         name: 'Кунгурская ледяная пещера',
         link: './images/kungur.jpg'
